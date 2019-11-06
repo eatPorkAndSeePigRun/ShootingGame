144a1d1b5773c01659139dd50d033a63d08e39a5
eatPorkAndSeePigRun <811302964@qq.com>
2019-11-06 15:00:48 +0800

    enemy spawn

---
+++ Assets/Scripts/EnemySpawn.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[AddComponentMenu("MyGame/EnemySpawn")]
+public class EnemySpawn : MonoBehaviour
+{
+    public Transform m_enemyPrefab;
+
+    void Start()
+    {
+        StartCoroutine(SpawnEnemy());
+    }
+
+    IEnumerator SpawnEnemy()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(Random.Range(5, 15));
+            Instantiate(m_enemyPrefab, transform.position, Quaternion.identity);
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawIcon(transform.position, "item.png", true);
+    }
+}
