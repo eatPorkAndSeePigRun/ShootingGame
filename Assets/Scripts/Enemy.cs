640e2106ba3294fa7f0bf4aab41d37f45fbe50fc
eatPorkAndSeePigRun <811302964@qq.com>
2019-11-06 09:37:59 +0800

    creating enemy

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[AddComponentMenu("MyGame/Enemy")]
+public class Enemy : MonoBehaviour
+{
+    public float m_speed = 1;
+    public int m_life = 10;
+    protected float m_rotSpeed = 30;
+
+
+    void Update()
+    {
+        UpdateMove();
+    }
+
+    protected virtual void UpdateMove()
+    {
+        float rx = Mathf.Sin(Time.time) * Time.deltaTime;
+        transform.Translate(new Vector3(rx, 0, -m_speed * Time.deltaTime));
+    }
+}
