087bbcc5eac6a3446e389950ad598c8255fd6554
eatPorkAndSeePigRun <811302964@qq.com>
2019-11-07 12:14:34 +0800

    level skip

---
+++ Assets/Scripts/TitleSceen.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+[AddComponentMenu("MyGame/TitleScreen")]
+public class TitleSceen : MonoBehaviour
+{
+    public void OnButtonGameStart()
+    {
+        SceneManager.LoadScene("level1");
+    }
+}
