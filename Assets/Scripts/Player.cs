aba03f952e93e8b58cc3eb8663f7acede24bd21c
eatPorkAndSeePigRun <811302964@qq.com>
2019-11-08 08:58:52 +0800

    controlling the protagonist with the mouse

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -16,28 +16,34 @@ public class Player : MonoBehaviour
     public Transform m_rocket;
     float m_rocketTimer = 0;
 
+    protected Vector3 m_targetPos;
+    public LayerMask m_inputMask;
+
+
     void Start()
     {
         m_transform = this.transform;
         m_audio = this.GetComponent<AudioSource>();
+        m_targetPos = this.m_transform.position;
     }
 
     void Update()
     {
-        float movev = 0;
-        float moveh = 0;
+        //float movev = 0;
+        //float moveh = 0;
 
-        if (Input.GetKey(KeyCode.UpArrow))
-            movev += m_speed * Time.deltaTime;
-        if (Input.GetKey(KeyCode.DownArrow))
-            movev -= m_speed * Time.deltaTime;
-        if (Input.GetKey(KeyCode.LeftArrow))
-            moveh -= m_speed * Time.deltaTime;
-        if (Input.GetKey(KeyCode.RightArrow))
-            moveh += m_speed * Time.deltaTime;
+        //if (Input.GetKey(KeyCode.UpArrow))
+        //    movev += m_speed * Time.deltaTime;
+        //if (Input.GetKey(KeyCode.DownArrow))
+        //    movev -= m_speed * Time.deltaTime;
+        //if (Input.GetKey(KeyCode.LeftArrow))
+        //    moveh -= m_speed * Time.deltaTime;
+        //if (Input.GetKey(KeyCode.RightArrow))
+        //    moveh += m_speed * Time.deltaTime;
 
-        m_transform.Translate(new Vector3(moveh, 0, movev));
+        //m_transform.Translate(new Vector3(moveh, 0, movev));
 
+        MoveTo();
 
         m_rocketTimer -= Time.deltaTime;
         if (m_rocketTimer <= 0)
@@ -48,9 +54,7 @@ public class Player : MonoBehaviour
                 Instantiate(m_rocket, m_transform.position, m_transform.rotation);
                 m_audio.PlayOneShot(m_shootClip);
             }
-
         }
-
     }
 
     private void OnTriggerEnter(Collider other)
@@ -64,7 +68,21 @@ public class Player : MonoBehaviour
                 Instantiate(m_explosionFX, m_transform.position, Quaternion.identity);
                 Destroy(this.gameObject);
             }
-
         }
+    }
+
+    void MoveTo()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            Vector3 ms = Input.mousePosition;
+            Ray ray = Camera.main.ScreenPointToRay(ms);
+            RaycastHit hitinfo;
+            bool iscast = Physics.Raycast(ray, out hitinfo, 1000, m_inputMask);
+            if (iscast)
+                m_targetPos = hitinfo.point;
+        }
+        Vector3 pos = Vector3.MoveTowards(m_transform.position, m_targetPos, m_speed * Time.deltaTime);
+        this.m_transform.position = pos;
     }
 }
